2cbbabe55e8d87af82ef225fe0d29208506cfc58
Md Sana <mailmdsana@gmail.com>
2026-04-28 12:08:27 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int z=0,o=0,t=0;
+        for(int i=0;i<nums.length;i++){
+         if(nums[i] == 0) z++;
+         else if(nums[i] == 1)o++;
+         else t++;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(z > 0){
+                nums[i] = 0;
+                z--;
+            }
+            else if(o > 0){
+                nums[i] = 1;
+                o--;
+            }
+            else{
+                nums[i] = 2;
+                t--;
+            }
+        }
+    }
+}
