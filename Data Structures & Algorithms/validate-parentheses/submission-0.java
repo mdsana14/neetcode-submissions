09663a67d3c9c61c1d95223666f82b2779c7d2a4
Md Sana <mailmdsana@gmail.com>
2026-04-30 14:01:24 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> r = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                r.push(')');
+            }
+            else if(c == '['){
+                r.push(']');
+            }
+            else if(c == '{'){
+                r.push('}');
+            }
+            else if(r.isEmpty() || r.pop() != c){
+                return false;
+            }
+        }
+        return r.isEmpty();
+    }
+}
